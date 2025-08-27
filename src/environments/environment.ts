ff5f82a6de5b406e657a5ac8540615cd83ed3313
Phumlani Arendse <aphumlani.dev@gmail.com>
2025-08-27 13:53:27 +0200

    added public api key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,18 +1,11 @@
 export const environment = {
   firebase: {
-    apiKey: '-SE',
-    authDomain: 'firebaseapp.com',
+    apiKey: 'AIzaSyAE40sPvWZmjlP22TMADTA3PWfuQyGh-SE',
+    authDomain: 'portfolio-blog-79bf6.firebaseapp.com',
     projectId: 'portfolio-blog-79bf6',
-<<<<<<< HEAD:src/environments/environment.ts
     storageBucket: 'portfolio-blog-79bf6.firebasestorage.app',
     messagingSenderId: '169991423661',
     appId: '1:169991423661:web:c3e1d0c3a961b6c06ea824',
     measurementId: 'G-HNPKTKWX77',
-=======
-    storageBucket: 'firebasestorage.app',
-    messagingSenderId: '1:',
-    appId: '1::web:abcdef',
-    measurementId: 'G-',
->>>>>>> a0dedb885f8111f9b8a024ba716c3f83e1704ddc:src/app/environments/environment.ts
   },
 };
