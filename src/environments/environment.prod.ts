b4e198fdb1b9619a104a73405063059b6ed3390a
Phumlani Arendse <aphumlani.dev@gmail.com>
2026-03-17 22:42:36 +0200

    added workflow_dispatch and update configs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
+  production: true,
   firebase: {
-    apiKey: '', // filled by CI/CD
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
-    measurementId: '',
+    apiKey: import.meta.env['VITE_FIREBASE_API_KEY'] ?? '', // filled by CI/CD
+    authDomain: import.meta.env['VITE_FIREBASE_AUTH_DOMAIN'] ?? '',
+    projectId: import.meta.env['VITE_FIREBASE_PROJECT_ID'] ?? '',
+    storageBucket: import.meta.env['VITE_FIREBASE_STORAGE_BUCKET'] ?? '',
+    messagingSenderId: import.meta.env['VITE_FIREBASE_MESSAGING_SENDER_ID'] ?? '',
+    appId: import.meta.env['VITE_FIREBASE_APP_ID'] ?? '',
+    measurementId: import.meta.env['VITE_FIREBASE_MEASUREMENT_ID'] ?? '',
   },
 };
